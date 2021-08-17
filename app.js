e4aefac7b8074e18f243fc25466ff910f5476b44
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-08-17 00:55:18 -0400

    setup canvas

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-// 1. game loop & logic
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
 
-// 2. class for Hero
-// 3. class for enemies
-// 4. start the game
+ctx.font = "10px Georgia";
+ctx.strokeText("Hello World!", 10, 50);
+
+ctx.moveTo(100, 300)
+ctx.lineTo(200, 300)
+ctx.stroke()
+
+ctx.fillStyle = '#e23435'
+ctx.fillRect(300, 400, 20, 45)
